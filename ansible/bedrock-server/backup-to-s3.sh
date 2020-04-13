d5bfab271d368f90d4677d72663bfff205141828
Ryan Price <ryapric@gmail.com>
2020-04-13 14:36:28 -0500

    Modify backup script to only run if data has been modified recently

--- ansible/bedrock-server/backup-to-s3.sh
+++ ansible/bedrock-server/backup-to-s3.sh
@@ -4,7 +4,12 @@ accountNumber=$(aws sts get-caller-identity --query Account --output text)
 bucket="minecraft-bedrock-server-${accountNumber}"
 bakfile="bedrock-server-backup.tar.gz"
 
-docker exec bedrock_server sh -c "tar -czf ${bakfile} worlds"
-docker cp "bedrock_server:/root/${bakfile}" "/root/${bakfile}"
-aws s3 mb "s3://${bucket}" || printf "Backup bucket ${bucket} already exists\n"
-aws s3 cp "/root/${bakfile}" "s3://${bucket}/${bakfile}"
+# Only backup to S3 if files were actually modified in the last 15 minutes. This
+# should save on S3 transfer costs.
+modifiedFiles=$(docker exec bedrock_server sh -c "find /root/worlds -mmin -15 -type f -print")
+if [[ "${modifiedFiles}" ]]; then
+  docker exec bedrock_server sh -c "tar -czf ${bakfile} worlds"
+  docker cp "bedrock_server:/root/${bakfile}" "/root/${bakfile}"
+  aws s3 mb "s3://${bucket}" || printf "Backup bucket ${bucket} already exists\n"
+  aws s3 cp "/root/${bakfile}" "s3://${bucket}/${bakfile}"
+fi
