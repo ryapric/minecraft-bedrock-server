80c0a203de33c6c6de50d6933691162cf55ebdf7
Ryan J. Price <ryapric@gmail.com>
2020-05-10 16:12:15 -0500

    Mostly structure daemon.py

--- bedrock-server/scripts/daemon.py
+++ bedrock-server/scripts/daemon.py
@@ -4,33 +4,55 @@ import boto3
 import datetime
 import time
 
-client = boto3.client('logs')
+def start_worker_instance():
+    pass
+# end start_worker_instance
 
-response = client.get_log_events(
-    logGroupName = '/aws/ec2/minecraft-bedrock-server',
-    logStreamName = 'bedrock-server',
-    limit = 1
-)
 
-last_logtime_actual = response['events'][0]['timestamp']
+def stop_worker_instance():
+    pass
+# end stop_worker_instance
 
-# Read in last seen logtime, and write out the last actual logtime to replace it
-with open('/mnt/efs/last_logtime_known', 'r+') as f:
-  last_logtime_known = int(f.read())
-  f.write(last_logtime_actual)
 
-if last_logtime_actual > last_logtime_known:
-  try:
-    start_worker_instance()
-  except SomeException as e:
-    print(e)
-else:
-  if worker_is_on():
+def worker_is_on():
+    pass
+# end worker_is_on
+
+
+def logs_say_do_something():
+    client = boto3.client('logs')
+
     try:
-      stop_worker_instance()
+        response = client.get_log_events(
+            logGroupName='/aws/ec2/minecraft-bedrock-server',
+            logStreamName='bedrock-server',
+            limit=1
+        )
     except ClientException as e:
-      print(e)
+        print(e)
 
-print(last_logtime_actual)
+    last_logtime_actual = response['events'][0]['timestamp']
 
-time.sleep(5)
+    # Read in last seen logtime, and write out the last actual logtime to replace it
+    with open('/mnt/efs/last_logtime_known', 'r+') as f:
+        last_logtime_known = int(f.read())
+        f.write(last_logtime_actual)
+
+    if last_logtime_actual > last_logtime_known:
+        return True
+    else:
+        return False
+# end logs_say_do_something
+
+
+def main():
+    if logs_say_do_something():
+        start_worker_instance()
+    else:
+        if worker_is_on():
+            stop_worker_instance()
+    time.sleep(5)
+# end main
+
+if __name__ == '__main__':
+    main()
